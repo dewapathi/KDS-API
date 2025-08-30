926bbc9340777e4785eeac095be32d93f52e387e
Pradeepa Lakruwan <pradeepa@creatit.com.au>
2025-08-30 11:39:18 +0530

    test pipe line.

--- core/views.py
+++ core/views.py
@@ -28,3 +28,5 @@ class SignInView(APIView):
         if serializer.is_valid():
             return Response(serializer.validated_data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
